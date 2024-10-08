881bd8f8a5063d617dcaeff2e0cc4918f73d12ef
bluebarryz <bzbear16@hotmail.com>
2024-10-07 22:20:07 -0400

    Turn TurtleClearer into a service

--- include/training/TurtleClearer.hpp
+++ include/training/TurtleClearer.hpp
@@ -3,7 +3,7 @@
 #include <memory>
 #include <string>
 #include <vector>
-
+#include <training/srv/reset.hpp>
 
 namespace composition {
 
@@ -11,8 +11,12 @@ class TurtleClearer : public rclcpp::Node {
 public:
     explicit TurtleClearer(const rclcpp::NodeOptions &options);
 private:
-    void clear_turtle();
+    void clear_turtle(const std::shared_ptr<training::srv::Reset::Request> request,
+        std::shared_ptr<training::srv::Reset::Response> response);
+
     rclcpp::Client<turtlesim::srv::Kill>::SharedPtr client;
+    rclcpp::Service<training::srv::Reset>::SharedPtr server;
+
     std::vector<std::string> turtle_names = {"turtle1", "moving_turtle",
                                            "stationary_turtle"};
 };
--- src/TurtleClearer.cpp
+++ src/TurtleClearer.cpp
@@ -2,16 +2,21 @@
 #include <chrono>
 
 using namespace std::chrono_literals;
+using namespace std::placeholders;
 
 namespace composition {
 
 TurtleClearer::TurtleClearer(const rclcpp::NodeOptions &options) : Node("turtle_clearer", options) {
+    server = this->create_service<training::srv::Reset>("/clear_turtle",
+        std::bind(&TurtleClearer::clear_turtle, this, _1, _2));
     client = this->create_client<turtlesim::srv::Kill>("/kill");
-
-    clear_turtle();
 }
 
-void TurtleClearer::clear_turtle() {
+void TurtleClearer::clear_turtle(const std::shared_ptr<training::srv::Reset::Request> request,
+    std::shared_ptr<training::srv::Reset::Response> response) {
+    
+    (void)request;
+
     while (!client->wait_for_service(1s)) {
         if (!rclcpp::ok()) {
         RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
@@ -20,16 +25,18 @@ void TurtleClearer::clear_turtle() {
     }
 
     for (std::string &name : turtle_names) {
-        auto request = std::make_shared<turtlesim::srv::Kill::Request>();
-        request->name = name;
-        RCLCPP_INFO(this->get_logger(), "%s", request->name.c_str());
-        auto result = client->async_send_request(request);
-        if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == rclcpp::FutureReturnCode::SUCCESS) {
-            RCLCPP_INFO(this->get_logger(), "Successfully cleared %s", name.c_str());
-        } else {
-            RCLCPP_ERROR(this->get_logger(), "Failed to clear %s", name.c_str());
-        }
+        auto req = std::make_shared<turtlesim::srv::Kill::Request>();
+        req->name = name;
+        RCLCPP_INFO(this->get_logger(), "%s", req->name.c_str());
+        auto result = client->async_send_request(req);
+        // if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) == rclcpp::FutureReturnCode::SUCCESS) {
+        //     RCLCPP_INFO(this->get_logger(), "Successfully cleared %s", name.c_str());
+        // } else {
+        //     RCLCPP_ERROR(this->get_logger(), "Failed to clear %s", name.c_str());
+        // }
     }
+
+    response->success = true;
 }
 
 } // namespace composition
